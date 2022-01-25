fa8a2b59a2eee3cc048eaae4e29c781388770829
Vo Quang Hoa <hvquang@openwt.com>
2022-01-25 23:59:50 +0700

    Run script to run migration/metadata/seed

--- seeds/postgres/1643125591457_init_seed.sql
+++ seeds/postgres/1643125591457_init_seed.sql
@@ -1,4 +1,5 @@
 SET check_function_bodies = false;
+DELETE FROM public.long_tails;
 INSERT INTO public.long_tails (tail, json_id) VALUES ('front', 1);
 INSERT INTO public.long_tails (tail, json_id) VALUES ('data', 2);
 INSERT INTO public.long_tails (tail, json_id) VALUES ('api', 3);
