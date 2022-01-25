7290fb8cd445ea50c2f709e89183d5de67c5724a
Vo Quang Hoa <hvquang@openwt.com>
2022-01-26 01:06:41 +0700

    Add wait for make sure hasura started

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 docker-compose up -d --build
 
-sleep 3
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8080/healthz)" != "200" ]]; do sleep 5; done'
 
 hasura metadata apply
 hasura migrate apply --database-name postgres
